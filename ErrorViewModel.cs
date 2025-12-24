ac32adf40dde7968dacf156612e863d0d6d7d9dd
Oftond <161725771+Oftond@users.noreply.github.com>
2025-12-24 15:44:55 +0700

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RussianCuisine.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
